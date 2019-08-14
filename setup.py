50b7f906e01c182a6d96b26524252fd5fc78bfdd
Danny Antaki <dantaki@ucsd.edu>
2019-08-14 16:48:17 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name='pbsmaker',
-	version='0.0.3',
+	version='0.0.4',
 	url='https://github.com/dantaki/pbsmaker',
 	author='Danny Antaki',
 	author_email='dantaki@ucsd.edu',
